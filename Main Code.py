280402c62241907c9ca6b0b1af94c4ee4aa16cb3
Liliana Ao <39006746+aozhangl@users.noreply.github.com>
2018-08-15 12:06:57 -0400

    Update Main Code.py

--- Main Code.py
+++ Main Code.py
@@ -14,7 +14,7 @@ def name_to_number(name):
     elif (name == "scissors"):
         return 4
     else:
-        return "Please enter a valid name."
+        return "Please enter a valid choice."
 
 def number_to_name(number):
     if (number == 0):
@@ -33,21 +33,25 @@ def number_to_name(number):
 def rpsls(player_choice): 
     import random
     print 
-    print "Player chooses " + player_choice + "."
-    player_number = name_to_number (player_choice)
-    comp_number = random.randrange (0, 5)
-    comp_choice = number_to_name (comp_number)
-    print "Computer chooses " + comp_choice + "."
-    remainder = (comp_number - player_number) % 5
-    if ((remainder == 1) or (remainder == 2)):
-        print "Computer wins!"
-    elif ((remainder == 3) or (remainder == 4)):
-        print "Player wins!"
-    else:
-        print "Player and computer tie!"
+    if name_to_number (player_choice) == "Please enter a valid choice.":
+        print name_to_number (player_choice)
+    else: 
+        print "Player chooses " + player_choice + "."
+        player_number = name_to_number (player_choice)
+        comp_number = random.randrange (0, 5)
+        comp_choice = number_to_name (comp_number)
+        print "Computer chooses " + comp_choice + "."
+        remainder = (comp_number - player_number) % 5
+        if ((remainder == 1) or (remainder == 2)):
+            print "Computer wins!"
+        elif ((remainder == 3) or (remainder == 4)):
+            print "Player wins!"
+        else:
+            print "Player and computer tie!"
     
 rpsls("rock")
 rpsls("Spock")
 rpsls("paper")
 rpsls("lizard")
 rpsls("scissors")
+rpsls("dynamite")
